389faf3bd6478551f4c172ed502ce40dc7f2e2c5
Handika Saputra <handikasaputra484@gmail.com>
2026-09-24 09:31:00 +0700

    Form Pendaftaran Sederhana

--- Form Pendaftaran Mahasiswa (sederhana)/script.js
+++ Form Pendaftaran Mahasiswa (sederhana)/script.js
@@ -1,2 +1,17 @@
 const form = document.getElementById("form");
-const nama = document.getElementsByClassName("nama");
+const nama = document.getElementById("nama");
+const email = document.getElementById("email");
+const jurusan = document.getElementById("jurusan");
+const semester = document.getElementById("semester");
+const hasil = document.getElementById("hasil");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    hasil.innerHTML =
+    "Nama: " + nama.value + "<br>" + "<br>" +
+    "Email Aktif: " + email.value + "<br>" + "<br>" +
+    "Jurusan: " + jurusan.value + "<br>" + "<br>" +
+    "Semester: " + semester.value + "<br>" + "<br>";
+});
